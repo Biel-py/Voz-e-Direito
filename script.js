1bb3b51b4cac1c5d2e4a34474b056d67740fbc82
Biel-py <gabriel.alvess2257@gmail.com>
2026-07-24 09:21:50 -0300

    Add back-to-top button functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const backToTopButton = document.querySelector(".botao_home");
 
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 250) {
+
+        backToTopButton.classList.add("mostrar");
+
+    } else {
+
+        backToTopButton.classList.remove("mostrar");
+    }
+});
+
+
+backToTopButton.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
